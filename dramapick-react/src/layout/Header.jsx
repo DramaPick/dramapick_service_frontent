b97077454ca0036ac5dc0f62cae32b4473a0cb37
soyoung2022 <yuns8671@gmail.com>
2024-10-30 11:42:37 +0900

    [ADD] 헤더 서비스명 클릭 시 메인 페이지로 전환

--- dramapick-react/src/layout/Header.jsx
+++ dramapick-react/src/layout/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import styles from "./Header.module.css";
 
 const Header = () => {
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate('/'); // 메인 페이지로 이동
+    };
+
     return (
         <header className={styles.header}>
-            <h1>DramaPick</h1>
+            <h1 onClick={handleClick}>DramaPick</h1>
         </header>
     );
 };
