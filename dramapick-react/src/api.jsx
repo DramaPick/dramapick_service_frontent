ba211b604569a1ac814f4962f8652922aec5cc97
soyoung2022 <yuns8671@gmail.com>
2025-03-02 02:12:33 +0900

    새로운 Dockerfile (5)

--- dramapick-react/src/api.jsx
+++ dramapick-react/src/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // axios 인스턴스 생성
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:8000"
+  baseURL: process.env.REACT_APP_API_URL || "https://dramapick.site/api"
 });
 
 export default api;
