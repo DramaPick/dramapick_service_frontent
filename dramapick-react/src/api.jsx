75050b74db1c13ed4b7753b59e56c11410f3c6f3
soyoung2022 <yuns8671@gmail.com>
2025-03-02 02:19:16 +0900

    새로운 deploy.yml

--- dramapick-react/src/api.jsx
+++ dramapick-react/src/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // axios 인스턴스 생성
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://dramapick.site/api"
+  baseURL: process.env.REACT_APP_API_URL || "https://dramapick.site"
 });
 
 export default api;
